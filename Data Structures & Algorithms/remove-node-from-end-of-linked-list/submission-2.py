0658d287e90320bacf0781ffd7306f7ff8265900
Saad Patel <psaad2988@gmail.com>
2026-07-28 23:43:05 +0530

    Add: remove-node-from-end-of-linked-list - submission-2

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-2.py
@@ -0,0 +1,32 @@
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        temp = head
+        cnt = 0
+        while temp:
+            cnt+=1
+            temp = temp.next
+
+        if cnt ==1:
+            return
+
+        if cnt == n:
+            return head.next   
+
+        prev = None
+        curr = head
+        i = 0
+        for i in range(cnt):
+            if i == (cnt-n): 
+                        
+                prev.next = curr.next
+            else:
+                prev = curr
+                curr = curr.next    
+               
+        return head
+          
+
+
+                 
+                
+
